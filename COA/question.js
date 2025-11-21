b74c01454c60d32785b0026ef62dc95bb37601b5
AR2908 <162954754+AR2908@users.noreply.github.com>
2025-11-21 19:34:33 +0530

    Add files via upload
    okay

---
+++ COA/question.js
@@ -0,0 +1,56 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is Full Form of GND pin in 8086",
+    answer: "Ground for Internel Cercuit",
+    options: [
+      "Graphite User Interpp.",
+      "Grphics user Input",
+      "Ground Input Circuit",
+      "Ground for Internel Cercuit",
+    ],
+  },
+  {
+    numb: 2,
+    question: "How Many Pin in microprocesser 8086",
+    answer: "40",
+    options: ["10", "20", "30", "40"],
+  },
+  {
+    numb: 3,
+    question:
+      "RQ/GT......... fill in the blank?",
+    answer: "Signal",
+    options: [
+      "Signal",
+      "Sort",
+      "Single sort",
+      "All of the above",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "Which of the following is known as a set of entities of the same type that share same properties, or attributes?",
+    answer: "Entity set",
+    options: ["Relation set", "Tuples", "Entity set", "Entity Relation model"],
+  },
+  {
+    numb: 5,
+    question: "Which command is used to remove a relation from an SQL ?",
+    answer: "Drop table",
+    options: ["Drop table", "Delete", "Purge", "Remove"],
+  },
+  {
+    numb: 6,
+    question: "Please, Rate this Portal!",
+    answer: "Good",
+    options: ["Good", "Need some improvement", "Not good", "None"],
+  },
+  {
+    numb: 7,
+    question: "Please, Rate this Portal!",
+    answer: "None",
+    options: ["Good", "Need some improvement", "Not good", "None"],
+  },
+];
