136bef7a0169052bcf86b313fa659d398a5b57fc
AR2908 <162954754+AR2908@users.noreply.github.com>
2025-11-21 21:38:45 +0530

    Update question.js

--- COA/question.js
+++ COA/question.js
@@ -1,56 +1,57 @@
 let questions = [
   {
     numb: 1,
-    question: "What is Full Form of GND pin in 8086",
-    answer: "Ground for Internel Cercuit",
+    question: "What is the main advantage of pipelining in processor design?",
+    answer: "Increased instruction throughput",
     options: [
-      "Graphite User Interpp.",
-      "Grphics user Input",
-      "Ground Input Circuit",
-      "Ground for Internel Cercuit",
+      "Decreased hardware cost",
+      "Simpler control unit",
+      "Increased instruction throughput",
+      "Lower clock speed"
     ],
   },
   {
     numb: 2,
-    question: "How Many Pin in microprocesser 8086",
-    answer: "40",
-    options: ["10", "20", "30", "40"],
+    question: "Which one of the following is a characteristic of RISC architecture?",
+    answer: "Fixed length instructions",
+    options: [
+      "Variable length instructions",
+      "Complex addressing modes",
+      "Fixed length instructions",
+      "Many microcoded instructions"
+    ],
   },
   {
     numb: 3,
-    question:
-      "RQ/GT......... fill in the blank?",
-    answer: "Signal",
+    question: "CISC processors typically have:",
+    answer: "More complex instructions",
     options: [
-      "Signal",
-      "Sort",
-      "Single sort",
-      "All of the above",
+      "Fewer clock cycles per instruction",
+      "More complex instructions",
+      "Simple hardwired control",
+      "Less instruction set"
     ],
   },
   {
     numb: 4,
-    question:
-      "Which of the following is known as a set of entities of the same type that share same properties, or attributes?",
-    answer: "Entity set",
-    options: ["Relation set", "Tuples", "Entity set", "Entity Relation model"],
+    question: "What does a pipeline 'bubble' indicate?",
+    answer: "A stalled clock cycle with no useful work",
+    options: [
+      "Instruction fetch",
+      "Branch prediction success",
+      "A stalled clock cycle with no useful work",
+      "More instructions in decode stage"
+    ],
   },
   {
     numb: 5,
-    question: "Which command is used to remove a relation from an SQL ?",
-    answer: "Drop table",
-    options: ["Drop table", "Delete", "Purge", "Remove"],
-  },
-  {
-    numb: 6,
-    question: "Please, Rate this Portal!",
-    answer: "Good",
-    options: ["Good", "Need some improvement", "Not good", "None"],
-  },
-  {
-    numb: 7,
-    question: "Please, Rate this Portal!",
-    answer: "None",
-    options: ["Good", "Need some improvement", "Not good", "None"],
+    question: "Which processor type is most associated with easy compiler optimizations?",
+    answer: "RISC",
+    options: [
+      "CISC",
+      "RISC",
+      "DSP",
+      "x86"
+    ],
   },
 ];
